94bc856b4015bad128bf5342925ed4f53d80d01d
AlaaMatouk <contact.alaamatouk@gmail.com>
2025-07-31 05:21:29 +0300

    .

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,8 +1,18 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../firebaseKey.json.json');
+const admin = require("firebase-admin");
+require("dotenv").config();
+
+if (!process.env.FIREBASE_KEY_JSON) {
+  throw new Error("Missing FIREBASE_KEY_JSON in environment variables");
+}
+
+let serviceAccount;
+serviceAccount = JSON.parse(process.env.FIREBASE_KEY_JSON);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
+
 const db = admin.firestore();
 module.exports = db;
